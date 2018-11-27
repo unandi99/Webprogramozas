9a9e554d0efefcaa28e1cd3a04ff7b348eb0c856
Ujfalusi Nándor <ujfalusinandor@gmail.com>
2018-11-27 11:08:28 +0100

    JavaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,12 @@
+"use strict";
+// Ez egy sorstintű megjegyzés (comment)
+console.log('Üdv a fedélzeten!');
+alert('Ez egy üzenet a liftből'); // ez is sorszintű, a parancs után
+
+/*
+Több soron
+keresztüli
+megjegyzés
+más néven
+blokk-szintű megjegyzés
+*/
